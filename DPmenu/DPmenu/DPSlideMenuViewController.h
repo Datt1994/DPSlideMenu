ef5a98b70f4172c8b29040c1bda037b8cd88ea87
Datt1994 <datt1994@gmail.com>
2016-05-31 11:23:52 -0700

    Update DPSlideMenuViewController.h

--- DPmenu/DPmenu/DPSlideMenuViewController.h
+++ DPmenu/DPmenu/DPSlideMenuViewController.h
@@ -1,22 +1,13 @@
-//
-//  SlideMenuViewController.h
-//  ShiponK
-//
-//  Created by datt on 28/02/1938 SAKA.
-//  Copyright © 1938 SAKA com.zaptechsolution. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
 @interface DPSlideMenuViewController : UIViewController
 
 
--(BOOL)menuBtnClick:(UIViewController *)m;
+-(void)menuBtnClick:(UIViewController *)m;
 -(void)tap;
 -(void)initSwipeAndTap:(UIViewController *)m;
--(void)PushBtnClick:(NSString *)str ViewController:(UIViewController*)vc
-;
--(void)initView:(NSString *)str ViewController:(UIViewController*)vc
-;
+-(void)PushBtnClick:(NSString *)str ViewController:(UIViewController*)vc;
+-(void)initView:(NSString *)str ViewController:(UIViewController*)vc;
 
 @end
