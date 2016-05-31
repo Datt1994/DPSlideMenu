04c0717a5e7707ca19cd30ccb14eb1ab723faf71
Datt1994 <datt1994@gmail.com>
2016-05-31 11:36:12 -0700

    Update view3_ViewController.m

--- DPmenu/DPmenu/view3_ViewController.m
+++ DPmenu/DPmenu/view3_ViewController.m
@@ -1,10 +1,3 @@
-//
-//  view3_ViewController.m
-//  DPmenu
-//
-//  Created by datt on 09/03/1938 SAKA.
-//  Copyright © 1938 SAKA datt. All rights reserved.
-//
 
 #import "view3_ViewController.h"
 #import "DPSlideMenuViewController.h"
