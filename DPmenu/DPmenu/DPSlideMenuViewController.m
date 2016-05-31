403edf68c9be147df7eda0a507ff8856311ef72c
Datt1994 <datt1994@gmail.com>
2016-05-31 13:50:21 -0700

    Add files via upload

--- DPmenu/DPmenu/DPSlideMenuViewController.m
+++ DPmenu/DPmenu/DPSlideMenuViewController.m
@@ -140,7 +140,7 @@
     
 }
 
--(void)PushBtnClick:(NSString *)str ViewController:(UIViewController*)vc
+-(void)PushBtnClick:(NSString *)str ViewController:(UIViewController*)vc BouncingAnimation:(BOOL)bounc
 {
     [objViewController.view removeFromSuperview];
     
@@ -152,22 +152,63 @@
     
     
     objViewController = (UIViewController *) [[UIStoryboard storyboardWithName:@"Main" bundle:nil]        instantiateViewControllerWithIdentifier:str] ;
-    
     [vc.view addSubview:objViewController.view];
     [vc addChildViewController:objViewController];
     [objViewController didMoveToParentViewController:vc];
+   
+
+    
+    if (bounc==YES) {
+        CGFloat midWidth = vc.view.frame.size.width*.5;
+        CAKeyframeAnimation *animation = [[self class] dockBounceAnimationWithViewWidth:midWidth];
+        [objViewController.view.layer addAnimation:animation forKey:@"bouncing"];
+        
+    }
+    else{
+        
+    
+        CABasicAnimation *animation = [CABasicAnimation animation];
+        animation.keyPath = @"position.x";
+        animation.fromValue = @(vc.view.frame.size.width+80);
+        animation.toValue = @(vc.view.frame.size.width/2);
+        animation.duration = .35;
+        
+        [objViewController.view.layer addAnimation:animation forKey:@"basic"];
+
+    }
+    
+
     
     
-    CABasicAnimation *animation = [CABasicAnimation animation];
-    animation.keyPath = @"position.x";
-    animation.fromValue = @(vc.view.frame.size.width+80);
-    animation.toValue = @(vc.view.frame.size.width/2);
-    animation.duration = .35;
     
-    [objViewController.view.layer addAnimation:animation forKey:@"basic"];
+}
+
++ (CAKeyframeAnimation*)dockBounceAnimationWithViewWidth:(CGFloat)viewWidth
+{
+    NSUInteger const kNumFactors    = 16;
+    CGFloat const kFactorsPerSec    = 30.0f;
+    CGFloat const kFactorsMaxValue  = 100.0f;
+    CGFloat factors[kNumFactors]    = {0,  60, 83, 100, 100, 83, 60, 32, 0, 0, 18, 28, 32, 28, 18, 0};
     
+    NSMutableArray* transforms = [NSMutableArray array];
     
+    for(NSUInteger i = 0; i < kNumFactors; i++)
+    {
+        CGFloat positionOffset  = factors[i] / kFactorsMaxValue * viewWidth;
+        CATransform3D transform = CATransform3DMakeTranslation(positionOffset,0.0f,  0.0f);
+        
+        [transforms addObject:[NSValue valueWithCATransform3D:transform]];
+    }
     
+    CAKeyframeAnimation* animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    animation.repeatCount           = 1;
+    animation.duration              = kNumFactors * 1.0f/kFactorsPerSec;
+    animation.fillMode              = kCAFillModeForwards;
+    animation.values                = transforms;
+    animation.removedOnCompletion   = YES; // final stage is equal to starting stage
+    animation.autoreverses          = NO;
+    
+    return animation;
 }
 
 @end
